f6ba759110fdeb762a197e6612a190d3a5f8c845
Corey Hinkle <Corey.S.Hinkle@leidos.com>
2019-12-20 15:25:39 -0500

    Add go files for webhook

---
+++ go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.13
+
+require (
+	github.com/alex-leonhardt/k8s-mutate-webhook v0.0.0-20190802065817-6c641d0280d9
+	github.com/gogo/protobuf v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	k8s.io/api v0.17.0
+	k8s.io/apimachinery v0.17.0
+)
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello %q", html.EscapeString(r.URL.Path))
+}
+
+func handleMutate(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s", err)
+	}
+
+	mutated, err := Mutate(body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s", err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(mutated)
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handleRoot)
+	mux.HandleFunc("/mutate", handleMutate)
+
+	s := &http.Server{
+		Addr:           ":8443",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServeTLS("./ssl/simple-webhook.pem", "./ssl/simple-webhook.key"))
+}
